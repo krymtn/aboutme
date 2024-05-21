e63bbd2b683ece408d6481fddb4280bd6111ec92
koray <koraymetin@windowslive.com>
2024-05-21 16:54:39 +0300

    manage/change theme mode with cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:aboutme/root/view/root_page.dart';
+import 'package:aboutme/theme/cubit/theme_cubit.dart';
 import 'package:flutter/material.dart';
 import 'theme/theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeMode? themeMode;
+  const MyApp({super.key, this.themeMode});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'aboutme.koraymetin',
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.dark,
-      home: const RootPage()
+    return BlocProvider(
+      create: (context) => ThemeCubit(themeMode ?? ThemeMode.light),
+      child: BlocSelector<ThemeCubit, dynamic, ThemeMode>(
+        builder: (context, state) {
+          return MaterialApp(
+              title: 'aboutme.koraymetin',
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              themeMode: state,
+              home: const RootPage()
+          );
+        }, selector: (state) => state,
+      ),
     );
   }
 }
--- lib/root/view/root_page.dart
+++ lib/root/view/root_page.dart
@@ -1,5 +1,7 @@
 import 'package:aboutme/base_screen.dart';
+import 'package:aboutme/theme/cubit/theme_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
@@ -8,7 +10,21 @@ class RootPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseScreen(
         builder: (context, constraints) {
-          return const Text('Koray Metin');
+          return Column(
+            children: [
+              const Text('Koray Metin'),
+              const SizedBox(height: 12.0),
+              BlocBuilder<ThemeCubit, dynamic>(
+                builder: (context, state) {
+                  return Switch(
+                      value: (state == ThemeMode.light),
+                      onChanged: (newValue) {
+                        (newValue) ?context.read<ThemeCubit>().themeToLight() :context.read<ThemeCubit>().themeToDark();
+                      });
+                }
+              ),
+            ],
+          );
         });
   }
 }
